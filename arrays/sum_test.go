ca96177f10b11d79c17c84d084d7b7c5d37ec3be
Marcio Bera <marciobera@hotmail.com>
2024-04-07 18:06:35 +0100

    refactor: move test to main test func

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,14 +3,12 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
-		got := Sum(numbers)
-		want := 15
+	got := Sum(numbers)
+	want := 15
 
-		if got != want {
-			t.Errorf("got %d want %d give, %v", got, want, numbers)
-		}
-	})
+	if got != want {
+		t.Errorf("got %d want %d give, %v", got, want, numbers)
+	}
 }
